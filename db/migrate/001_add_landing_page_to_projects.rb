a26478d475366c665ba2af57edd90ab0db9877a6
y503unavailable <y503unavailable@users.noreply.github.com>
2020-08-23 22:31:43 +0900

    #1 Fix db migration error (Redmine4)

--- db/migrate/001_add_landing_page_to_projects.rb
+++ db/migrate/001_add_landing_page_to_projects.rb
@@ -1,5 +1,14 @@
-class AddLandingPageToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :landing_page, :string
+class AddLandingPageToProjects < ActiveRecord::Migration[4.2]
+
+  def up
+    unless column_exists?(:projects, :landing_page, :string)
+      add_column :projects, :landing_page, :string
+    end
+  end
+  
+  def down
+    if column_exists?(:projects, :landing_page, :string)
+      remove_column :projects, :landing_page
+    end
   end
 end
--- db/migrate/002_add_landing_page_to_users.rb
+++ db/migrate/002_add_landing_page_to_users.rb
@@ -1,5 +1,18 @@
-class AddLandingPageToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :landing_page, :string
+class AddLandingPageToUsers < ActiveRecord::Migration[4.2]
+
+  def up
+    unless column_exists?(:users, :landing_page, :string)
+      add_column :users, :landing_page, :string
+    end
   end
+
+  def down
+    if column_exists?(:users, :landing_page, :string)
+      remove_column :users, :landing_page
+    end
+  end
+
 end
+
+
+
